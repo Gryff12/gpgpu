5ef8ecf5a27cee434c580917b01ea6bc9cb11c86
Maxime - Zachary <maxime.madrau@icloud.com>
2023-05-23 19:32:29 +0200

    Test Cuda + C++

--- Project/main.cu
+++ Project/main.cu
@@ -1,10 +1,19 @@
 #include <stdio.h>
+#include <iostream>
+#include "test.h"
+
 __global__ void print_kernel() {
 	printf(
 			"Hello from block %d, thread %d\n",
 			blockIdx.x, threadIdx.x);
 }
+
 int main() {
-	print_kernel<<<2, 3>>>();
+
+	hello();
+
+	print_kernel<<<3, 3>>>();
 	cudaDeviceSynchronize();
+
+	return 0;
 }
---
+++ Project/test.h
@@ -0,0 +1,10 @@
+//
+// Created by Maxime on 23/05/2023.
+//
+
+#ifndef PROJECT_TEST_H
+#define PROJECT_TEST_H
+
+void hello(void);
+
+#endif //PROJECT_TEST_H
