d3138e32b69a2fcb2ed20dae96d6c91c4a7d259c
Maxime - Zachary <maxime.madrau@icloud.com>
2023-06-01 16:14:11 +0200

    Preproc macro to not try to compile CUDA code in C++ & Fix casting errors

--- Project/src/Classification.hh
+++ Project/src/Classification.hh
@@ -20,6 +20,8 @@ int height,
 double threshold
 );
 
+#ifdef __CUDA_ARCH__
+
 __device__ void sortThreeValues(double &a, double &b, double &c);
 
 __device__ void swap(double &a, double &b);
@@ -28,5 +30,6 @@ __global__ void
 IsBackgroundPixelKernel(bool *d_retVal, size_t pitch_ret, double *d_textureSimilarity, size_t pitch_texture,
                         double *d_colorFeatures_r, size_t pitch_r, double *d_colorFeatures_g, size_t pitch_g,
                         double threshold, int width, int height);
+#endif
 
 #endif //PROJECT_CLASSIFICATION_HH
--- Project/src/TextureSimilarityMeasures.hh
+++ Project/src/TextureSimilarityMeasures.hh
@@ -12,8 +12,11 @@
 
 double **TextureSimilarityMeasures(Color **img1, Color **img2, int width, int height);
 
+
+#ifdef __CUDA_ARCH__
 __device__ uint8_t getPixel(Color *image, int x, int y, int width, int height);
 
 __global__ void TextureSimilarityKernel(Color *d_img1, size_t pitch1, Color *d_img2, size_t pitch2, double *d_similarities, size_t pitch, int width, int height);
+#endif
 
 #endif //PROJECT_TEXTURESIMILARITYMEASURES_HH
